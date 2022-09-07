a3154b146ed1864358e068426630d8a0a3bf32f2
Roisin O'Reilly <roisin.oreilly1996@gmail.com>
2022-09-07 17:03:50 +0100

    add metro config to read bin files

--- dog-react-native/metro.config.js
+++ dog-react-native/metro.config.js
@@ -1,6 +1,11 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-
-const { getDefaultConfig } = require("@expo/metro-config");
-const defaultConfig = getDefaultConfig(__dirname);
-defaultConfig.resolver.assetExts.push("cjs");
-module.exports = defaultConfig;
+const { getDefaultConfig } = require('metro-config');
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig();
+  const { assetExts } = defaultConfig.resolver;
+  return {
+    resolver: {
+      // Add bin to assetExts
+      assetExts: [...assetExts, 'bin', 'cjs'],
+    }
+  };
+})();
