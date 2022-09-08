50d8f8f1ded9925537a60a2b76a24161b53827da
pippaamy <pippa_austin@live.com>
2022-09-08 15:16:02 +0100

    added gifs for loading, added log out button

--- dog-react-native/screens/HomeScreen.js
+++ dog-react-native/screens/HomeScreen.js
@@ -1,13 +1,25 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 // import Navigation from "../Navigation";
 
 const HomeScreen = () => {
+  const handleLogOut = () => {
+    console.log("logging out");
+    navigation.replace("LogIn");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text onPress={() => navigation.navigate("Home")}>Home</Text>
-      {/* <Navigation /> */}
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text onPress={() => navigation.navigate("Home")}>Home</Text>
+        {/* <Navigation /> */}
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity OnPress={handleLogOut} style={styles.button}>
+          <Text style={styles.buttonText}>Log out</Text>
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
@@ -19,5 +31,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
   },
 });
