8f6cf28e9307bcac719b2a7d322b8fc557c0358b
pippaamy <pippa_austin@live.com>
2022-09-09 14:20:42 +0100

    styling on homescreen

--- dog-react-native/screens/HomeScreen.js
+++ dog-react-native/screens/HomeScreen.js
@@ -18,31 +18,36 @@ const HomeScreen = () => {
 
   return (
     <>
-      <Text style={styles.mainText}>
-        The pawsome app that allows you to collect dogs. Go for a walk, snap a
-        dog picture and add it to your collection. There are 30 dogs to collect!
-      </Text>
-      <Text style={styles.warning}>
-        Make sure you have permission to take a dog picture!
-      </Text>
-      <Image
-        source={require("../autmndognew.gif")}
-        style={{
-          width: 320,
-          height: 200,
-          display: "flex",
-          flexDirection: "row",
-        }}
-        resizeMode="contain"
-        resizeMethod="resize"
-        justifyContent="center"
-        alignItems="center"
-      />
-      <TouchableOpacity onPress={handleLogOut} style={styles.button}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Log out</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.mainText}>
+          The pawsome app that allows you to collect dogs. Go for a walk, snap a
+          dog picture and add it to your collection. Compare your collections to
+          your friends. There are 30 dogs to collect!
+        </Text>
+        <Text style={styles.warning}>
+          Make sure you have permission to take a dog picture!
+        </Text>
+        <Text>{"\n"}</Text>
+        <Image
+          source={require("../autmndognew.gif")}
+          style={{
+            width: 320,
+            height: 200,
+            display: "flex",
+            flexDirection: "row",
+          }}
+          resizeMode="contain"
+          resizeMethod="resize"
+          justifyContent="center"
+          alignItems="center"
+        />
+        <Text>{"\n"}</Text>
+        <TouchableOpacity onPress={handleLogOut} style={styles.button}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Log out</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </>
   );
 };
@@ -52,7 +57,7 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#f6d186",
     alignItems: "center",
     justifyContent: "center",
   },
@@ -60,7 +65,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#dc7646",
     width: "100%",
     padding: 15,
-    borderRadius: 10,
+    borderTopEndRadius: 10,
     alignItems: "center",
   },
 
@@ -69,12 +74,22 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     fontSize: 16,
   },
-  mainText: { color: "#a45c5c", fontWeight: "700", fontSize: 16, padding: 20 },
-  warning: {
-    color: "#b45c5c",
+  mainText: {
+    padding: 10,
+    textAlign: "center",
+    color: "#a45c5c",
     fontWeight: "700",
-    fontSize: 16,
-    padding: 20,
-    paddingBottom: 100,
+    fontSize: 18,
+    paddingBottom: 20,
+  },
+  warning: {
+    padding: 10,
+    borderWidth: 2.4,
+    borderColor: "red",
+    borderRadius: 10,
+    textAlign: "center",
+    color: "#b45c5c",
+    fontWeight: "bold",
+    fontSize: 18,
   },
 });
