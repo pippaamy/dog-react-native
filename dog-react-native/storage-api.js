9296781a84d5c5a9a4bbebbb6af1d675f2a638fa
shamel hok <msh_shamel@hotmail.co.uk>
2022-09-09 10:54:31 +0100

    add deleteImage, improve uploadImage function

--- dog-react-native/storage-api.js
+++ dog-react-native/storage-api.js
@@ -1,12 +1,18 @@
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import { addImagePath, addProfilePic } from "./api";
 
 const storage = getStorage();
 
 const imageRef = ref(storage, "image");
 
-function uploadImage(file, name) {
-  return uploadBytes(ref(storage, name), file)
+function uploadImage(file, name_make_it_unique) {
+  return uploadBytes(ref(storage, name_make_it_unique), file)
     .then((snapshot) => {
       console.log("Uploaded a  file!");
       return snapshot;
@@ -22,28 +28,36 @@ function getImageUrl(name) {
   });
 }
 
-function userUploadImage(file) {
-  const name = Date.now().toString();
-  uploadImage(file, name)
+function userUploadImage(file, uniqueName_eg_DateNow) {
+  return uploadImage(file, uniqueName_eg_DateNow)
     .then(() => {
-      addImagePath(name);
+      addImagePath(uniqueName_eg_DateNow);
     })
     .catch((error) => console.log({ error, code: error.code }));
 }
 
-function userUploadProfileImage(file) {
-  const name = Date.now().toString();
-  uploadImage(file, name)
+function userUploadProfileImage(file, uniqueName_eg_DateNow) {
+  return uploadImage(file, uniqueName_eg_DateNow)
     .then(() => {
-      addProfilePic(name);
+      addProfilePic(uniqueName_eg_DateNow);
     })
     .catch((error) => console.log({ error, code: error.code }));
 }
 
+function deleteImage(imageName) {
+  return deleteObject(ref(storage, imageName))
+    .then(() => {
+      console.log("File deleted successfully");
+    })
+    .catch((error) => {
+      console.log("Uh-oh, an error occurred!", { error });
+    });
+}
 export {
   imageRef,
   uploadImage,
   getImageUrl,
   userUploadImage,
   userUploadProfileImage,
+  deleteImage,
 };
