fb1deb438bbb526a59a760a5aee041151b052597
pippaamy <pippa_austin@live.com>
2022-09-13 15:11:59 +0100

    profile edit screen

--- dog-react-native/screens/PredictedDog.js
+++ dog-react-native/screens/PredictedDog.js
@@ -1,152 +1,130 @@
-import { View, Text, Image, TouchableOpacity } from "react-native"
-import { Button } from "react-native-elements"
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Button } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
-import { uploadImageFromUri, userUploadImage } from "../storage-api"
-import { StyleSheet } from "react-native"
-import { DogCard } from "./DogCard";
+import { uploadImageFromUri, userUploadImage } from "../storage-api";
+import { StyleSheet } from "react-native";
+// import { DogCard } from "./DogCard";
 import { useState } from "react";
-const dogs = require("../public/breeds-50-lower - breeds.json")
+const dogs = require("../public/breeds-50-lower - breeds.json");
 
+export const PredictedDog = ({ image, predictions }) => {
+  const [clicked, setClicked] = useState(false);
 
-export const PredictedDog = ({image, predictions}) => {
-const [clicked, setClicked] = useState(false)
+  let formattedPredictions = [];
+  const format = () => {
+    predictions.forEach((prediction) => {
+      for (let i = 0; i < Object.keys(dogs).length; i++) {
+        if (
+          prediction.className.toLowerCase().includes(dogs[i].nameLower) &&
+          prediction.probability > 0
+        ) {
+          formattedPredictions.push(dogs[i].breed, prediction.probability);
+        }
+      }
+    });
+  };
 
+  format();
+  const navigation = useNavigation();
+  const handleNo = () => {
+    navigation.replace("Camera");
+  };
 
-    let formattedPredictions = []
-    const format = () => {
-        predictions.forEach((prediction) => {
-            for(let i=0; i<Object.keys(dogs).length; i++) {
-                if (prediction.className.toLowerCase().includes(dogs[i].nameLower) && prediction.probability > 0) {
-                    formattedPredictions.push(dogs[i].breed, prediction.probability)
-                }
-            }
-        })
-    }
-    
-    format()
-    const navigation = useNavigation();
-    const handleNo = () => {
-        navigation.replace("Camera");
-    }
+  const saveUnmatched = () => {
+    const id = `__${Date.now().toString()}`;
+    uploadImageFromUri(image, id);
+  };
 
-    const saveUnmatched = () => {    
-        const id = `__${Date.now().toString()}`; 
-        uploadImageFromUri(image, id)
-    }
+  const handleYes = () => {
+    // uploadImageFromUri(image,predictions[0].className+`_${Date.now().toString()}`)
+    setClicked(true);
+  };
 
-       const handleYes = () => {
-        // uploadImageFromUri(image,predictions[0].className+`_${Date.now().toString()}`)
-        setClicked(true)
-       }
-
-
-        if (formattedPredictions.length !== 0) {
-            return (
-                <>
+  if (formattedPredictions.length !== 0) {
+    return (
+      <>
         <View style={style.container}>
-        <Image style={style.image} 
-        source={{uri: image}}/>
-            <Text style={style.mainText}>
-                There is a {(formattedPredictions[1] * 100).toFixed(1)}% chance that dog is a {formattedPredictions[0]}!
-            </Text>
-            <Text style={style.confirmText}>
-                Is that right?
-            </Text>
-            
-            <View style={style.buttonWrapper}>
+          <Image style={style.image} source={{ uri: image }} />
+          <Text style={style.mainText}>
+            There is a {(formattedPredictions[1] * 100).toFixed(1)}% chance that
+            dog is a {formattedPredictions[0]}!
+          </Text>
+          <Text style={style.confirmText}>Is that right?</Text>
 
-            <TouchableOpacity 
-            style={style.button}
-            onPress={handleYes}>
-                <Text style={style.buttonText}>
-                    Yes
-                </Text>
-                </TouchableOpacity> 
-                <TouchableOpacity 
-            style={style.button}
-            onPress={handleNo}>
-                <Text style={style.buttonText}>
-                    No
-                </Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                style={style.button} 
-                onPress={saveUnmatched}>
-                  <Text style={style.buttonText}>
-                    Save Anyway
-                  </Text>
-                </TouchableOpacity>
-                </View>
+          <View style={style.buttonWrapper}>
+            <TouchableOpacity style={style.button} onPress={handleYes}>
+              <Text style={style.buttonText}>Yes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.button} onPress={handleNo}>
+              <Text style={style.buttonText}>No</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.button} onPress={saveUnmatched}>
+              <Text style={style.buttonText}>Save Anyway</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-                    </>
-        )
-    }
-    if (formattedPredictions.length === 0) {
-        return (
-<>
-            <View style={style.container}>
-            <Image style={style.image} 
-        source={{uri: image}}/>
-            <Text style={style.errorText}>
-                We're not sure that is a dog! Try again or save your photo for later
-            </Text>
-            <TouchableOpacity 
-            style={style.button}
-            onPress={handleNo}>
-                <Text style={style.buttonText}>
-                    Try again
-                </Text>
-                </TouchableOpacity>
+      </>
+    );
+  }
+  if (formattedPredictions.length === 0) {
+    return (
+      <>
+        <View style={style.container}>
+          <Image style={style.image} source={{ uri: image }} />
+          <Text style={style.errorText}>
+            We're not sure that is a dog! Try again or save your photo for later
+          </Text>
+          <TouchableOpacity style={style.button} onPress={handleNo}>
+            <Text style={style.buttonText}>Try again</Text>
+          </TouchableOpacity>
         </View>
-
-    </>
-    )
-    }
-    
-}
+      </>
+    );
+  }
+};
 
 const style = StyleSheet.create({
-    button: {
-        backgroundColor: "#dc7646",
-        width: "30%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-        margin: 10
-      },
-      buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-      },
-    container: {
-        flex: 1,
-        backgroundColor: "#f6d186",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      buttonWrapper: {
-        flexDirection: "row"
-      },
-      image: {
-        width: 300,
-        height: 300,
-        borderRadius: 15,
-        margin: 10
-      },
-      confirmText: {
-        fontSize: 22
-      },
-      mainText: {
-        fontSize: 30,
-        // fontWeight: 500
-      },
-      errorText: {
-        fontSize: 30,
-        alignItems: "center",
-        marginLeft: 20,
-        marginRight: 20,
-        marginTop: 10,
-        marginBottom: 10
-      }
-})
+  button: {
+    backgroundColor: "#dc7646",
+    width: "30%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#f6d186",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonWrapper: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 15,
+    margin: 10,
+  },
+  confirmText: {
+    fontSize: 22,
+  },
+  mainText: {
+    fontSize: 30,
+    // fontWeight: 500
+  },
+  errorText: {
+    fontSize: 30,
+    alignItems: "center",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+});
