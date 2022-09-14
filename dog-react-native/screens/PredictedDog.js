287deb9c2c148253a34a2864f4e04b557dbddd53
pippaamy <pippa_austin@live.com>
2022-09-14 11:35:18 +0100

    i need to chnage this before pulling

--- dog-react-native/screens/PredictedDog.js
+++ dog-react-native/screens/PredictedDog.js
@@ -3,43 +3,47 @@ import { Button } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 import { uploadImageFromUri, userUploadImage } from "../storage-api";
 import { StyleSheet } from "react-native";
-// import { DogCard } from "./DogCard";
+import { DogCard } from "./DogCard";
 import { useState } from "react";
 const dogs = require("../public/breeds-50-lower - breeds.json");
 
 export const PredictedDog = ({ image, predictions }) => {
   const [clicked, setClicked] = useState(false);
 
+  let formattedPredictions = [];
+  const format = () => {
+    predictions.forEach((prediction) => {
+      for (let i = 0; i < Object.keys(dogs).length; i++) {
+        if (
+          prediction.className.toLowerCase().includes(dogs[i].nameLower) &&
+          prediction.probability > 0.2
+        ) {
+          formattedPredictions.push(dogs[i].breed, prediction.probability);
+        }
+      }
+    });
+  };
 
-    let formattedPredictions = []
-    const format = () => {
-        predictions.forEach((prediction) => {
-            for(let i=0; i<Object.keys(dogs).length; i++) {
-                if (prediction.className.toLowerCase().includes(dogs[i].nameLower) && prediction.probability > 0.2) {
-                    formattedPredictions.push(dogs[i].breed, prediction.probability)
-                }
-            }
-        })
-    }
-    
-    format()
-    const navigation = useNavigation();
-    const handleNo = () => {
-        navigation.replace("Camera");
-    }
+  format();
+  const navigation = useNavigation();
+  const handleNo = () => {
+    navigation.replace("Camera");
+  };
 
-    const saveUnmatched = () => {    
-        const id = `__${Date.now().toString()}`; 
-        uploadImageFromUri(image, "test12345678865432")
-    }
+  const saveUnmatched = () => {
+    const id = `__${Date.now().toString()}`;
+    uploadImageFromUri(image, "test12345678865432");
+  };
 
-       const handleYes = () => {
-        setClicked(true)
-       }
+  const handleYes = () => {
+    setClicked(true);
+  };
 
-if (clicked) {
-    return <DogCard image={image} formattedPredictions={formattedPredictions}/>
-}
+  if (clicked) {
+    return (
+      <DogCard image={image} formattedPredictions={formattedPredictions} />
+    );
+  }
 
   if (formattedPredictions.length !== 0) {
     return (
@@ -64,97 +68,86 @@ if (clicked) {
             </TouchableOpacity>
           </View>
         </View>
-                    </>
-        )
-    }
-    if (formattedPredictions.length === 0) {
-        return (
-<>
-            <View style={style.container}>
-            <Image style={style.image} 
-        source={{uri: image}}/>
-            <Text style={style.errorText}>
-                We're not sure that is a dog! Try again or save your photo for later
-            </Text>
-                <View style={style.buttonWrapper}>
-            <TouchableOpacity 
-            style={style.errorButton}
-            onPress={handleNo}>
-                <Text style={style.errorButtonText}>
-                    Try again
-                </Text>
-                </TouchableOpacity>
-                <TouchableOpacity 
-                style={style.errorButton} 
-                onPress={saveUnmatched}>
-                  <Text style={style.errorButtonText}>
-                    Save Anyway
-                    
-                
-                  </Text>
-                </TouchableOpacity>
-                    </View>
-
-    </>
-    )
-        }
-}
+      </>
+    );
+  }
+  if (formattedPredictions.length === 0) {
+    return (
+      <>
+        <View style={style.container}>
+          <Image style={style.image} source={{ uri: image }} />
+          <Text style={style.errorText}>
+            We're not sure that is a dog! Try again or save your photo for later
+          </Text>
+          <View style={style.buttonWrapper}>
+            <TouchableOpacity style={style.errorButton} onPress={handleNo}>
+              <Text style={style.errorButtonText}>Try again</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={style.errorButton} onPress={saveUnmatched}>
+              <Text style={style.errorButtonText}>Save Anyway</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </>
+    );
+  }
+};
 
 const style = StyleSheet.create({
-    button: {
-        backgroundColor: "#dc7646",
-        width: "30%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-        margin: 10
-      },
-      buttonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 16,
-      },
-    container: {
-        flex: 1,
-        backgroundColor: "#f6d186",
-        alignItems: "center",
-        justifyContent: "center",
-      },
-      buttonWrapper: {
-        flexDirection: "row"
-      },
-      image: {
-        width: 300,
-        height: 300,
-        borderRadius: 15,
-        margin: 10
-      },
-      confirmText: {
-        fontSize: 22
-      },
-      mainText: {
-        fontSize: 30,
-        // fontWeight: 500
-      },
-      errorText: {
-        fontSize: 30,
-        alignItems: "center",
-        marginLeft: 23,
-        marginRight: 23,
-        marginTop: 10,
-        marginBottom: 10
-      }, 
-      errorButton: {
-        backgroundColor: "#dc7646",
-        width: "40%",
-        padding: 15,
-        borderRadius: 10,
-        alignItems: "center",
-        margin: 10
-      }, 
-      errorButtonText: {
-        color: "white",
-        fontWeight: "700",
-        fontSize: 18,
-      }
-})
+  button: {
+    backgroundColor: "#dc7646",
+    width: "30%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    margin: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#f6d186",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonWrapper: {
+    flexDirection: "row",
+  },
+  image: {
+    width: 300,
+    height: 300,
+    borderRadius: 15,
+    margin: 10,
+  },
+  confirmText: {
+    fontSize: 22,
+  },
+  mainText: {
+    fontSize: 30,
+    // fontWeight: 500
+  },
+  errorText: {
+    fontSize: 30,
+    alignItems: "center",
+    marginLeft: 23,
+    marginRight: 23,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  errorButton: {
+    backgroundColor: "#dc7646",
+    width: "40%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    margin: 10,
+  },
+  errorButtonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 18,
+  },
+});
